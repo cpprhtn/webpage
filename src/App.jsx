b8c6da3b5067c6c261f6ef698cfbc1f399d552b0
Junwon Lee <cpprhtn@naver.com>
2025-01-22 22:32:03 +0900

    Update CV

--- src/App.jsx
+++ src/App.jsx
@@ -58,11 +58,11 @@ function App() {
         <img
           src="/src/assets/me.jpg"
           alt="Profile"
-          className="w-24 h-24 rounded-full border-4 border-white"
+          className="w-24 h-24 rounded-full border-4 border-white mt-12"
         />
         
         <p className="text-4xl font-semibold">Junwon Lee</p>
-        <p className="text-2xl opacity-100">《Data Analyst》</p>
+        {/* <p className="text-2xl opacity-100">《Data Analyst》</p> */}
 
         {/* 아이콘 그룹 */}
         <div className="flex space-x-4 mt-6">
@@ -101,25 +101,55 @@ function App() {
           </a>
         </div>
 
+        <div className="max-w-screen-md mx-auto">
+          {/* 프로젝트 그룹 */}
+          <div className="grid grid-cols-2 gap-4 mt-4">
+            {[
+              { title: "12년간의 다양한 개발 경험", subtitle: "AI(Pytorch, Tensorflow), MLOps(Airflow), ETL(Pandas, Polars), 웹(Front, Backend)" },
+              { title: "대규모 데이터 분석", subtitle: "4TB 규모의 해양 데이터 처리 및 분석, 2GB * 100개의 공장 Raw 데이터 처리 및 분석" },
+              { title: "4년간의 연구 경험", subtitle: "3개의 AI 논문 1저자 및 1건의 프로그램 특허" },
+              { title: "ETRI(한국전자통신연구원) 인턴", subtitle: "환경ICT연구실에서 공장 에너지 관리 시스템 표준화 연구 참여" }
+            ]
+            .map((item, idx) => (
+              <div
+                key={idx}
+                className="bg-black bg-opacity-15 p-8 rounded-xl hover:bg-opacity-20 transition"
+              >
+                <h3 className="text-lg font-semibold">{item.title}</h3>
+                <p className="text-sm text-opacity-80">{item.subtitle}</p>
+              </div>
+            ))}
+          </div>
 
-        {/* 프로젝트 그룹 */}
-        <div className="grid grid-cols-2 gap-4 mt-4">
-          {[
-            { title: "12년간의 다양한 개발 경험", subtitle: "AI(Pytorch, Tensorflow), MLOps(Airflow), ETL(Pandas, Polars), 웹(Front, Backend)" },
-            { title: "대규모 데이터 분석", subtitle: "4TB 규모의 해양 데이터 처리 및 분석, 2GB * 100개의 공장 Raw 데이터 처리 및 분석" },
-            { title: "4년간의 연구 경험", subtitle: "3개의 AI 논문 1저자 및 1건의 프로그램 특허" },
-            { title: "ETRI(한국전자통신연구원) 인턴", subtitle: "환경ICT연구실에서 공장 에너지 관리 시스템 표준화 연구 참여" }
-          ]
-          .map((item, idx) => (
-            <div
-              key={idx}
-              className="bg-white bg-opacity-15 p-8 rounded-xl hover:bg-opacity-20 transition"
-            >
-              <h3 className="text-lg font-semibold">{item.title}</h3>
-              <p className="text-sm text-opacity-80">{item.subtitle}</p>
+          {/* 학력 및 이력 */}
+          <div className="mt-8">
+            <h2 className="text-2xl font-bold mb-4">학력 및 이력</h2>
+            <div className="grid grid-cols-1 gap-4 mt-4">
+              {[
+                { title: "학력", subtitle: "동아대학교 AI 전공, 2~4학년 과대표/2~3학년 학생회, MLP(Mecca-of-Learning-Pioneer) 학술동아리 설립, ETRI 한국전자통신연구원 연구연수생, 부산대학교 Wireless Networks Lab(WINE) 외부연구원, 부경대학교 해양역학·예측 연구실 외부연구원" },
+                { title: "자격증", subtitle: "[NVIDIA] Fundamentals of Deep Learning, [Tensorflow] Tensorflow Developer Certificate" },
+                { title: "논문 & 특허", subtitle: "[한국통신학회/1저자] miniEDA: 데이터 메시를 활용한 통합 데이터 분석 및 품질 관리 플랫폼, [한국멀티미디어학회/1저자] 클라우드 기반 웹사이트의 탄소 배출 저감 방안 연구, [한국멀티미디어학회/1저자] Anti-UAV 시스템을 위한 UAV 신호 분류 CNN 모델, [컴퓨터프로그램저작물 저작권] 통계기법을 이용한 해수면온도 관측자료의 이상치 검출 프로그램" },
+                { title: "수상이력", subtitle: "[부산테크노파크] 지역현안해결 대학수업지원 프로젝트 우수상, [부산광역시/과학기술정보통신부] 부산 제 7회 ICT 해커톤 대상, [한국멀티미디어학회/인공지능그랜드ICT연구센터] 인공지능 캡스톤디자인 경진대회 우수상, [부산광역시] 부산 제 5회 ICT 해커톤 최우수상, [한국정보과학진흥협회] ICT K-awards 알고리즘 프로그래밍 고등부 장려상, [교내] DevDay AI-SW 실증역량경진대회 경진대회 대상" },
+                { title: "교육이수사항", subtitle: "부산정보영재교육원(부산시교육청/2년), 고교 소프트웨어 인재교육(부산대학교/142시간), 데이터 크리에이터 캠프(한국정보화진흥원/60시간)" },
+                { title: "Organizers", subtitle: "Python Korea Organizer, C++ Korea Organizer, Airflow Korea Organizer, Seasonal University Study & Communication(SUSC) Co-Founder, Google Developer Student Clubs(GDSC) Core Member, Pytorch 한국 사용자 모임 Moderator" },
+                { title: "특이사항", subtitle: "PyTorch 기여 경험, 혼자 공부하는 컴퓨터구조 + 운영체제 베타리더, ChatGPT Reverse Engineering(한국인공지능연구소)" },
+              ].map((item, idx) => (
+                <div
+                  key={idx}
+                  className="bg-black bg-opacity-15 p-8 rounded-xl hover:bg-opacity-20 transition flex justify-between items-start"
+                >
+                  <h3 className="text-lg font-semibold text-left w-48">{item.title}</h3>
+                  <div className="text-sm text-opacity-80 w-[100%] text-left overflow-x-auto whitespace-nowrap custom-scrollbar">
+                    {item.subtitle.split(",").map((subItem, subIdx) => (
+                      <div key={subIdx}>{subItem.trim()}</div>
+                    ))}
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
+
 
         <footer className="custom-footer">
           <p>© 2025 by cpprhtn</p>
